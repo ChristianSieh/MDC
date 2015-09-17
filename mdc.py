be501c26e9bf9e86f575dcea7859885d94c757d3
Sieh <1960681@linux103.mcs.sdsmt.edu>
2015-09-17 09:44:24 -0600

    Fixed some errors and can now go through and get the min and max value for each column. TODO: Clean up variable names

--- mdc.py
+++ mdc.py
@@ -42,15 +42,36 @@ labels = reader.__next__()
 print(labels)
 
 '''*Should* give us a list of zeroes equal to the number of labels-2
-Since we don't need the sample number of class for normalizing'''
+Since we don't need the sample number or class for normalizing'''
 minimum = [0] * (len(labels) - 2)
-maximum = [0] * (len(lables) - 2)
+maximum = [0] * (len(labels) - 2)
 
 'Iterator to go through each column so we can get min/max values'
-iterator = range(3, len(labels))
+iterator = range(2, len(labels))
 
 'Get all the values'
 values = csv.reader(fin)
 
+'rowValues1(terrible name) is the first row of data for the data set'
+rowValues1 = values.__next__()
+
+'''Initialize the minimum and maximum arrays with
+the first row'''
+for i in range(2, len(labels)):
+	minimum[i - 2] = rowValues1[i]
+	maximum[i - 2] = rowValues1[i]	
+
+print(minimum)
+print(maximum)
+
+'''Go through the rest of the file to find the minimum and
+maximum values for each column'''
 for labelValues in values:
-	print(labelValues)
+	for i in range(2, len(labels)):
+		if labelValues[i] < minimum[i - 2]:
+			minimum [i - 2] = labelValues[i]
+		if labelValues[i] > maximum[i - 2]:
+			maximum [i - 2] = labelValues[i]
+		
+print(minimum)
+print(maximum)
